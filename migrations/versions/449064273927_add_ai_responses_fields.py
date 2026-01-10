20941f5a374fea34911c7ad1f160b66a29e0ea45
Woolfer0097 <Woolfer0097@yandex.ru>
2026-01-10 15:57:17 +0300

    fix: migration

--- migrations/versions/449064273927_add_ai_responses_fields.py
+++ migrations/versions/449064273927_add_ai_responses_fields.py
@@ -1,6 +1,6 @@
 """add_ai_responses_fields
 
-Revision ID: 449064273927
+Revision ID: 0f68606a7afd
 Revises: 0acee5582538
 Create Date: 2026-01-08 12:00:00.000000
 
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = '449064273927'
+revision: str = '0f68606a7afd'
 down_revision: Union[str, None] = '0acee5582538'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
